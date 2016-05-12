955e35e258de0d179bdd7ef4f71ef1501894f2f0
marian-nita <nita.marian@ymail.com>
2016-05-12 11:22:10 +0300

    testting list examples

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-print("Hello, GitHub!")
+myCats = []
 
-print("Hello again, dear GitHub!")
+while True:
+    print("Print the name of the cat " + str(len(myCats) + 1) + " (or enter nothing to stop): ")
+    name = input()
+    if name == '':
+        break
+    myCats += [name]
+
+print("The name of the cats are: ")
+for cat in myCats:
+    print(" " + cat)
+
+check = input("Enter a name of the cat: ")
+if check in myCats:
+    print(check + " is one of my cats.")
+else:
+    print(check + " is not my cat.")
