447848ccb4a4ea83d264132bddb13ec0a653c1ac
marian-nita <nita.marian@ymail.com>
2016-05-22 19:58:14 +0300

    commaCode example final result .

--- commaCode.py
+++ commaCode.py
@@ -1,12 +1,16 @@
 spam = ['apples', 'bananas', 'tofu', 'cats']
 
+
+def rreplace(s, old, new, occurence):
+    li = s.rsplit(old, occurence)
+    return new.join(li)
+
 def list2str(list):
     list.insert(len(list) - 1, 'and')
     string = ''
     for i in list:
         string += str(i) + ', '
-    newString = string.replace(',', '')
-    lastString = newString.replace(' ', ', ', len(list) - 2)
-    return lastString
+    newString = rreplace(string, ',', '', len(list) - 2)
+    return newString
 
 print(list2str(spam))
