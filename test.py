b021a70cd3473abe1a7c89a8e5894a5e132f93c4
marian-nita <nita.marian@ymail.com>
2016-05-26 10:49:53 +0300

    ticTacToe fist version - needs completion

--- test.py
+++ test.py
@@ -1,18 +1,10 @@
-myCats = []
 
-while True:
-    print("Print the name of the cat " + str(len(myCats) + 1) + " (or enter nothing to stop): ")
-    name = input()
-    if name == '':
-        break
-    myCats += [name]
+import pprint
+message = 'It was a bright cold day in April, and the clock were striking thirteen'
+count = {}
 
-print("The name of the cats are: ")
-for cat in myCats:
-    print(" " + cat)
+for character in message:
+    count.setdefault(character, 0)
+    count[character] += 1
 
-check = input("Enter a name of the cat: ")
-if check in myCats:
-    print(check + " is one of my cats.")
-else:
-    print(check + " is not my cat.")
+pprint.pprint(count)
